526b8dd20f333474c36d757698ac49ad7c1ab32a
FoxowDV <65514843+FoxowDV@users.noreply.github.com>
2025-02-22 13:28:51 -0700

    add things

--- pong/main.cpp
+++ pong/main.cpp
@@ -1,3 +1,5 @@
+#include "shader.hpp"
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
@@ -22,6 +24,10 @@ int main()
 
 	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT,"OpenGL", NULL, NULL);
 
+
+
+
+
 	if (window == NULL) {
 		std::cout << "Error while creating GLFW window" << std::endl;
 		glfwTerminate();
@@ -38,16 +44,71 @@ int main()
 		return -1;
 	}
 
+	Shader shader1("shaders/vertexShader.glsl", "shaders/color.glsl");
+
+	// figure
+	GLfloat vertices[] = {
+		-.5f, 0.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+		-.25f, .5f, 0.0f,		0.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+		.25f, .5f, 0.0f,		0.0f, 0.0f, 0.0f,
+		.5f, 0.0f, 0.0f,		0.0f, 0.0f, 0.0f
+	};
+
+	GLuint indices[] = {
+		0, 1, 2,
+		1, 2, 3,
+		2, 3, 4
+	};
+
+
+	GLuint VBO, VAO, EBO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	// Vertices
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// Position attibute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// Color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		shader1.use();
 
-	
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 
 	return 0;
 }
